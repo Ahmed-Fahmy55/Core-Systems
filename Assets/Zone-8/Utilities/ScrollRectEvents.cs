b7ecb83a419a2180f606ac381bcef5165259fcea
Ahmed Fahmy <ahmed.fahmy@bltzo.com>
2025-11-26 16:18:21 +0200

    Fix scroll event
    Bug where if text was too small it docent work

--- Assets/Zone-8/Utilities/ScrollRectEvents.cs
+++ Assets/Zone-8/Utilities/ScrollRectEvents.cs
@@ -1,13 +1,15 @@
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-namespace Zone8.Utilities
+namespace Bltzo.Utilities
 {
     [RequireComponent(typeof(ScrollRect))]
     public class ScrollRectEvents : MonoBehaviour
     {
-        public UnityEvent EndReached;
+        public UnityEvent _endReached;
+
         private ScrollRect _scrollRect;
 
 
@@ -16,12 +18,33 @@ namespace Zone8.Utilities
             _scrollRect = GetComponent<ScrollRect>();
             _scrollRect.onValueChanged.AddListener(OnScrollValueChanged);
         }
+        private void Start()
+        {
+            // Wait for UI layout to be fully calculated
+            StartCoroutine(CheckImmediateEnd());
+        }
+
+        private IEnumerator CheckImmediateEnd()
+        {
+            // Wait one frame so layout elements finish sizing
+            yield return null;
+            Canvas.ForceUpdateCanvases();
+
+            var viewport = _scrollRect.viewport.rect.height;
+            var content = _scrollRect.content.rect.height;
+
+            // If content is same size or smaller → we're already "at the end"
+            if (content <= viewport)
+            {
+                _endReached?.Invoke();
+            }
+        }
 
         private void OnScrollValueChanged(Vector2 value)
         {
             if (value.y <= .01f)
             {
-                EndReached?.Invoke();
+                _endReached?.Invoke();
             }
         }
 
