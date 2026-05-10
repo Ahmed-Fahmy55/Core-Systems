227a9d5ee85ca220a00fef865ca54fd68f5dbc59
Ahmed Fahmy <ahmed.fahmy@bltzo.com>
2026-05-10 14:41:53 +0300

    Removing extra systems

--- Assets/_Project/Scripts/Question System/Runtime/UI/Views/SingleChoiceQuestionView.cs
+++
@@ -1,9 +0,0 @@
-
-using Zone8.Question.Core;
-
-namespace Zone8.Question.Runtime.UI.Views
-{
-    public class SingleChoiceQuestionView : ChoiceQuestionViewBase<SingleChoiceQuestion>
-    {
-    }
-}
